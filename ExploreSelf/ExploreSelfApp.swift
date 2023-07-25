3c7c29712a3e5b1deedde034bdcf3135a2c6fc7b
Christian Leinberger <christian.leinberger.1@study.thws.de>
2023-07-25 12:59:57 +0200

    Fix

--- ExploreSelf/ExploreSelfApp.swift
+++ ExploreSelf/ExploreSelfApp.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 @main
 struct ExploreSelfApp: App {
-    let persistenceController = PersistenceController.shared
+   
 
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 TestView()
-                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                
             }
         }
     }
